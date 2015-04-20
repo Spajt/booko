133c6026f7168048f445b8a5ccab39c76614a349
Spajt <oskar.sajdak@gmail.com>
2015-04-20 11:36:38 -0400

    Zadania rekrutacyjne

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$file = file_get_contents("source.txt");
+$arr = array();
+preg_match_all('#t\(\"(.*?)\"\)#s', $file, $arr);
+$arr = $arr[1];
+/*for ($a = 0; $a < strlen($arr[0]); $a++) {
+    echo $a . ". " . ord($arr[0][$a]) . "<br />";
+}*/
+for ($a = 0; $a < count($arr); $a++) {
+    $arr[$a] = preg_replace('#\"\+\r\s{2,}\"#', "", $arr[$a]);
+}
+
+for ($a = 0; $a < count($arr); $a++) {
+    $arr[$a] = str_pad($arr[$a], strlen($arr[$a]) + 2, "\"", STR_PAD_BOTH);
+    echo "<br />" . $arr[$a] . "<br />"; // = preg_replace('#\"\+\r\s{2,}\"#',"",$arr[$a]);
+}
+
