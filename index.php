8b97230c7df8834ceb289e0b721c1856cdc3a8a9
Spajt <oskar.sajdak@gmail.com>
2015-04-20 11:54:05 -0400

    new quotes

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$file = file_get_contents("source.txt");
+$file = file_get_contents('source.txt');
 $arr = array();
 preg_match_all('#t\(\"(.*?)\"\)#s', $file, $arr);
 $arr = $arr[1];
@@ -8,11 +8,11 @@ $arr = $arr[1];
     echo $a . ". " . ord($arr[0][$a]) . "<br />";
 }*/
 for ($a = 0; $a < count($arr); $a++) {
-    $arr[$a] = preg_replace('#\"\+\r\s{2,}\"#', "", $arr[$a]);
+    $arr[$a] = preg_replace('#\"\+\r\s{2,}\"#', '', $arr[$a]);
 }
 
 for ($a = 0; $a < count($arr); $a++) {
-    $arr[$a] = str_pad($arr[$a], strlen($arr[$a]) + 2, "\"", STR_PAD_BOTH);
-    echo "<br />" . $arr[$a] . "<br />"; // = preg_replace('#\"\+\r\s{2,}\"#',"",$arr[$a]);
+    $arr[$a] = str_pad($arr[$a], strlen($arr[$a]) + 2, '"', STR_PAD_BOTH);
+    echo '<br />' . $arr[$a] . '<br />'; // = preg_replace('#\"\+\r\s{2,}\"#',"",$arr[$a]);
 }
 
